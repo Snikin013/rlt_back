9c2f24f2df7a9942dcca3219df2b93b449d212a4
Daniil <dan-013@yandex.ru>
2023-04-09 03:39:10 +0300

    second

--- app/handlers.py
+++ app/handlers.py
@@ -41,17 +41,78 @@ def take_data_second(user_form: check_inn = Body(..., embed=True), database=Depe
         limit 1
         ;'''.format(INN)
 
-    df = pd.read_sql(sql, connect_db())
+    sql_egrul = '''
+        select 
+            extract(year from now()) - extract(year from cast(registration_date as date)) as year, 
+            capital_size, okved_basic_code,  
+            now()::date -  cast(registration_date as date) as day_dif
+        from public.egrul_info
+        where inn = \'{}\'
+        limit 1'''.format(INN)
 
-    # if int(df['year']) < pd.datetime.now().year:
-    #     result['badlist'] = {status: 1, value: df['include_reason']}
-    #pd.datetime.now().month
+    sql_contr = '''
+        select customer_inn, count(*)
+        from public.contract_main_info
+        where customer_inn = \'{}\'
+        group by customer_inn
+        '''.format(INN)
+
+    df = pd.read_sql(sql, connect_db())
+    egrul = pd.read_sql(sql_egrul, connect_db())
+    egrul['capital_size'].fillna(0, inplace=True)
+    if egrul['capital_size'].values == ['']:
+        egrul['capital_size'] = 0
+    contr = pd.read_sql(sql_contr, connect_db())
+ 
+
+    if egrul.shape[0] == 1:
+        result['found'] = True
+        result['inn'] = INN
+        result['name'] = INN
+#-----------------------TIMELIFE-----------------------------------------------------------------
+        if int(egrul['year'].iloc[0]) < 1:
+            result['years'] = {'status': 1, 'value': '{} years'.format(int(int(egrul['day_dif']) / 365))}
+        elif int(egrul['year'].iloc[0]) >= 1 and int(egrul['year']) < 3:
+            result['years'] = {'status': 2, 'value': '{} years'.format(int(int(egrul['day_dif']) / 365))}
+        elif int(egrul['year'].iloc[0]) >= 3 and int(egrul['year']) < 10:
+            result['years'] = {'status': 3, 'value': '{} years'.format(int(int(egrul['day_dif']) / 365))}
+        elif int(egrul['year'].iloc[0]) >= 10:
+            result['years'] = {'status': 4, 'value': '{} years'.format(int(int(egrul['day_dif']) / 365))}
+#-----------------------CAPITAL-------------------------------------------------------------------
+        if int(egrul['capital_size'].iloc[0]) < 10**6:
+            result['capital'] = {'status': 1, 'value': '{} rub'.format(int(egrul['capital_size']))}
+        elif int(egrul['capital_size'].iloc[0]) >= 10**6 and int(egrul['capital_size']) < 10**9 * 2:
+            result['capital'] = {'status': 2, 'value': '{} rub'.format(int(egrul['capital_size']))}
+        elif int(egrul['capital_size'].iloc[0]) >= 10**9 * 2 and int(egrul['capital_size']) < 10**10:
+            result['capital'] = {'status': 3, 'value': '{} rub'.format(int(egrul['capital_size']))}
+        elif int(egrul['capital_size'].iloc[0]) >= 10**10:
+            result['capital'] = {'status': 4, 'value': '{} rub'.format(int(egrul['capital_size']))}
+#-----------------------CONTRACTS-----------------------------------------------------------------
+        if int(contr['count'].iloc[0]) <= 5:
+            result['contracts'] = {'status': 1, 'value': '{} контрактов'.format(int(contr['count']))}
+        elif int(contr['count'].iloc[0]) > 5 and int(contr['count']) < 50:
+            result['contracts'] = {'status': 2, 'value': '{} контрактов'.format(int(contr['count']))}
+        elif int(contr['count'].iloc[0]) >= 50 and int(contr['count']) < 500:
+            result['contracts'] = {'status': 3, 'value': '{} контрактов'.format(int(contr['count']))}
+        elif int(contr['count'].iloc[0]) >= 500:
+            result['contracts'] = {'status': 4, 'value': '{} контрактов'.format(int(contr['count']))}
+
+    else:
+        result['found'] = False
+
+
+    if df.shape[0] == 1:
+        if int(df['year']) < pd.datetime.now().year:
+            result['badlist'] = {'status': 1, 'value': df['include_reason']}
+    else:
+        result['badlist'] = {'status': 0, 'value': "Чист как белый лист"}
+
 
     #list(pd.read_sql(sql, connect_db()).to_dict('index').values())
 
     return {
             'found': True,
-            'inn': "1234567890",
+            'inn': INN,
             'name': 'ООО Ромашка',
             'years': {'status': 1, 'value': "3 месяца"},
             'capital': {'status': 3, 'values': "10 млрд"},
